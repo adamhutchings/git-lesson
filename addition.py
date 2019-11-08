ba460cc7123a394724a994257912d22ed73fd780
Adam Hutchings <adamhutchings@Adams-MacBook-Pro.local>
2019-11-07 19:26:03 -0800

    fix 'addition' function
    make unneccessarily complicated, but make addition work!

--- addition.py
+++ addition.py
@@ -1,4 +1,6 @@
-def addition(a, b):
-    return(a - b)
+import math
 
-print(addition(2, 3))
+def addition(a, b, c, d, e, f):
+    return(math.sqrt(a - (0 - abs(b))**(5-3))
+
+print(addition(2, 3, 1, 4, 5, 3))
